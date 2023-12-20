71e500d3f1a30e6a49dc3ebd2ab9c068a51facc9
Lucas Blanco <126288618+FixGlitch@users.noreply.github.com>
2023-12-20 15:58:06 -0300

    casi finish

--- src/Components/Proyects/Proyects.jsx
+++ src/Components/Proyects/Proyects.jsx
@@ -1,8 +1,43 @@
+import React from "react";
+import proyect1 from "../../assets/Proyect1.png";
+import proyect2 from "../../assets/Proyect2.png";
+import proyect3 from "../../assets/Proyect3.png";
+import styles from "../Proyects/Proyects.module.css";
+
 export default function Proyects() {
-    return (
-        <div id="proyects">
-            
-        </div>
-    )
-    
-};
+  const handleMouseOver = (event) => {
+    const target = event.target;
+    target.style.zIndex = "1";
+  };
+
+  const handleMouseOut = (event) => {
+    const target = event.target;
+    target.style.zIndex = "0";
+  };
+
+  return (
+    <div id="proyects" className={styles.projectsContainer}>
+      <img
+        className={`${styles.proyect} ${styles.proyect1}`}
+        src={proyect1}
+        alt="Rick and Morty"
+        onMouseOver={handleMouseOver}
+        onMouseOut={handleMouseOut}
+      />
+      <img
+        className={`${styles.proyect} ${styles.proyect2}`}
+        src={proyect2}
+        alt="Countriesapp"
+        onMouseOver={handleMouseOver}
+        onMouseOut={handleMouseOut}
+      />
+      <img
+        className={`${styles.proyect} ${styles.proyect3}`}
+        src={proyect3}
+        alt="GenshySupplements"
+        onMouseOver={handleMouseOver}
+        onMouseOut={handleMouseOut}
+      />
+    </div>
+  );
+}
